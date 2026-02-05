50f0916dde0dcf60ac44dabb3229d9e74968726b
CremenEngineer <84256320+Satyam998413@users.noreply.github.com>
2026-02-05 20:20:21 +0530

    add get data api

--- src/index.js
+++ src/index.js
@@ -1,55 +1,38 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const serverless = require("serverless-http");
 require("dotenv").config();
 const { httpLogger } = require("./lib/winstonLogger");
 
 const app = express();
 
-// CORS configuration
-const allowedOrigins = [
-  "https://cord4-ai-practical-fe.vercel.app",
-  "http://localhost:3000",
-];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like Postman) or allowed origins
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-// Apply CORS
-app.use(cors(corsOptions));
-
-// Explicitly handle preflight OPTIONS requests
-app.options("*", cors(corsOptions));
+// Middleware
+app.use(cors())
 
 app.use(express.json());
+
+// Custom Middleware to log request details and execution time
 app.use(httpLogger);
 
-// Root route
+
+// Root route → return index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-// API routes
+// Routes
 app.use("/api/image", require("./routes/image.routes"));
 app.use("/api/video", require("./routes/video.routes"));
 
-// Error handler
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-// Export for Vercel serverless
-module.exports = app;
-module.exports.handler = serverless(app);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
