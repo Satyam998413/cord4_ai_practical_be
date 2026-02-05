5589ff2ed17f88dfa471059cb503edd158b627f9
CremenEngineer <84256320+Satyam998413@users.noreply.github.com>
2026-02-05 20:10:15 +0530

    add get data api

--- src/index.js
+++ src/index.js
@@ -8,12 +8,7 @@ const app = express();
 
 
 // Middleware
-app.use(cors({
-  origin: "https://cord4-ai-practical-fe.vercel.app/",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-}));
+app.use(cors())
 
 app.use(express.json());
 
