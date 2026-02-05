1b1e58cc41fbc8e452f07687581fa6a8101e7622
CremenEngineer <84256320+Satyam998413@users.noreply.github.com>
2026-02-05 20:13:42 +0530

    add get data api

--- src/index.js
+++ src/index.js
@@ -1,38 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const serverless = require("serverless-http");
 require("dotenv").config();
 const { httpLogger } = require("./lib/winstonLogger");
 
 const app = express();
 
+const corsOptions = {
+  origin: ["https://cord4-ai-practical-fe.vercel.app", "http://localhost:3000"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
 
-// Middleware
-app.use(cors())
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
 
 app.use(express.json());
-
-// Custom Middleware to log request details and execution time
 app.use(httpLogger);
 
-
-// Root route → return index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-// Routes
 app.use("/api/image", require("./routes/image.routes"));
 app.use("/api/video", require("./routes/video.routes"));
 
-// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ message: "Something went wrong!" });
 });
 
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
+module.exports.handler = serverless(app);
